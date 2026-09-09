ef3812abc02ce3eae37c7cbcc16fb96147d9f36c
atuzarirwehappy <atuzarirwehappy@gmail.com>
2026-09-09 17:26:17 +0300

    Update population.cpp

--- Chapter_02/population.cpp
+++ Chapter_02/population.cpp
@@ -6,3 +6,18 @@
 // Write a program to display the population for each of the next five years. Assume the current population is 312032486 and one year has 365 days.
 
 
+#include <iostream>
+using namespace std;
+int main(){
+int current_population = 312032486;
+int seconds_per_year = 24 * 60 * 60 * 365;
+int births = seconds_per_year/7;
+int deaths = seconds_per_year/13;
+int immigrants = seconds_per_year/45;
+for(int year = 1; year <= 5 ;year++){
+current_population = current_population + births - deaths +immigrants;
+cout << "year "<< year << ":" << current_population << endl;
+}
+
+return 0;
+}
